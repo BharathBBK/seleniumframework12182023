c268222d5eebcbc31b9697be128abcf32211c0db
bharathk <bharathk@D4J0PT2.pyramidconsultinginc.com>
2023-12-18 21:30:08 +0530

    third commit

---
+++ src/test/java/com/tmb/tests/BaseTest.java
@@ -0,0 +1,33 @@
+package com.tmb.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void launchbrowser() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+	}
+	
+	@AfterTest
+	public void quitbrowser() {
+		
+		driver.quit();
+		
+	}
+
+}
--- src/test/java/com/tmb/tests/LoginPageTest.java
+++ src/test/java/com/tmb/tests/LoginPageTest.java
@@ -1,15 +1,28 @@
 package com.tmb.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class LoginPageTest {
+public class LoginPageTest extends BaseTest {
 	
 
-	@Test
+	@Test(priority = 2)
 	public void Test1() {
 		
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Basha",Keys.ENTER);
 		
+	    
+	}
+	
+	@Test(priority = 1)
+	public void Test2() {
+		driver.get("https://www.saucedemo.com/v1/index.html");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 	}
 	
 

ee883fa6cce1dfacb20e8d167a629225a7e92fe9
bharathk <bharathk@D4J0PT2.pyramidconsultinginc.com>
2023-12-18 10:50:19 +0530

    second commit

---
+++ src/test/java/com/tmb/tests/LoginPageTest.java
@@ -0,0 +1,16 @@
+package com.tmb.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+
+	@Test
+	public void Test1() {
+		
+		
+	}
+	
+
+}
